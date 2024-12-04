80fb32d2e7df93deeac51383c6374e8082bf22e4
demonnn911 <dmit.leush@gmail.com>
2024-12-04 16:49:58 +0300

    realised function for writing and reading messages from client, add methods to hub in separate goroutines

--- server/internal/hub/client.go
+++ server/internal/hub/client.go
@@ -2,6 +2,7 @@ package hub
 
 import (
 	"github.com/gorilla/websocket"
+	"github.com/sirupsen/logrus"
 )
 
 type Client struct {
@@ -16,4 +17,42 @@ type Message struct {
 	Content  string `json:"content"`
 	RoomID   int64  `json:"roomid"`
 	Username string `json:"username"`
+}
+
+func (c *Client) writeMessages() {
+	defer func() {
+		err := c.Conn.Close()
+		logrus.Error("writeMessage", err)
+	}()
+	for {
+		message, ok := <-c.Message
+		if !ok {
+			return
+		}
+		c.Conn.WriteJSON(message)
+	}
+}
+
+func (c *Client) readMessages(hub *Hub) {
+	defer func() {
+		hub.Unregister <- c
+		err := c.Conn.Close()
+		logrus.Error("readMessage", err)
+	}()
+	for {
+		_, m, err := c.Conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				logrus.Error(err)
+			}
+			logrus.Error(err)
+			break
+		}
+		msg := &Message{
+			Content:  string(m),
+			RoomID:   c.RoomID,
+			Username: c.Username,
+		}
+		hub.Broadcast <- msg
+	}
 }
--- server/internal/hub/ws_handler.go
+++ server/internal/hub/ws_handler.go
@@ -1,5 +1,14 @@
 package hub
 
+import (
+	"net/http"
+	"server/internal/utils/response"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
 type WSHandler struct {
 	hub *Hub
 }
@@ -13,4 +22,66 @@ func NewWSHandler(hub *Hub) *WSHandler {
 type createRoomReq struct {
 	ID   int64  `json:"id"`
 	Name string `json:"name"`
+}
+
+func (w *WSHandler) CreateRoom(c *gin.Context) {
+	var input createRoomReq
+	if err := c.BindJSON(&input); err != nil {
+		response.NewErrorResponse(c, http.StatusBadRequest, "invalid input body")
+		return
+	}
+
+	w.hub.Rooms[input.ID] = &Room{
+		ID:      input.ID,
+		Name:    input.Name,
+		Clients: map[int64]*Client{},
+	}
+	c.JSON(http.StatusOK, response.NewStatusResponse("you succesfully created a room"))
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func (w *WSHandler) JoinRoom(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		response.NewErrorResponse(c, http.StatusBadRequest, "couldn't establish connection to room")
+		return
+	}
+	roomID, err := strconv.ParseInt(c.Param("roomID"), 10, 64)
+	if err != nil {
+		response.NewErrorResponse(c, http.StatusBadRequest, "incorrect room id")
+		return
+	}
+	clientID, err := strconv.ParseInt(c.Query("userID"), 10, 64)
+	if err != nil {
+		response.NewErrorResponse(c, http.StatusBadRequest, "incorrect client id")
+		return
+	}
+	username := c.Query("username")
+
+	cl := &Client{
+		Conn:     conn,
+		Message:  make(chan Message),
+		RoomID:   roomID,
+		ID:       clientID,
+		Username: username,
+	}
+
+	msg := &Message{
+		Content:  "user " + username + " has joined the room",
+		RoomID:   roomID,
+		Username: username,
+	}
+
+	w.hub.Register <- cl
+	w.hub.Broadcast <- msg
+	go cl.writeMessages()
+	cl.readMessages(w.hub)
+
 }
